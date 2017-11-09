879f5f3a65134a6bcba424d2d8454c441d015113
wals38 <wals.38100@gmail.com>
2017-11-09 17:53:58 +0100

    Database export [tests-symfony.sql]

---
+++ tests-symfony.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 09 nov. 2017 à 16:53
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tests-symfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `food_record`
+--
+
+DROP TABLE IF EXISTS `food_record`;
+CREATE TABLE IF NOT EXISTS `food_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recorded_at` date NOT NULL,
+  `entitled` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `calories` int(11) NOT NULL,
+  `username` longtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
